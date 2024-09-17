623acc4cdeec7a76f31a7acbcce3e0c616011847
RitwikSingh28 <itsritwik26@gmail.com>
2024-09-17 08:15:55 +0530

    modify asm code to look like C using fasm macros

--- web_in_asm/hello.asm
+++ web_in_asm/hello.asm
@@ -1,26 +1,37 @@
 format ELF64 executable
 
-segment readable writable executable
-entry main
-main:
+; writing the `write` syscall in C style using Macro Substitutions
+macro write fd, buf, count
+{
   ;Make a syscall for write, corresponding to int 1
   mov rax, 1    
   ;Provide the first argument in rdi, 1 -> stdout
-  mov rdi, 1
+  mov rdi, fd
   ;Provide the buffer contents in rsi
-  mov rsi, msg
+  mov rsi, buf
   ;Provide the size of the buffer in rdx
-  mov rdx, 13
+  mov rdx, count
   ; The above configuration is equivalent to the UNIX call
   ; write(1, "Hello World!", 13)
   syscall
+}
 
-  ; Syscall to exit
+; writing the `exit` syscall 
+macro exit code
+{
   mov rax, 60
   ; Put the exit code in rdi
-  mov rdi, 0
+  mov rdi, code
   syscall
+}
 
+segment readable writable executable
+entry main
+main:
+  write 1, msg, msg_len
+  exit 0
+  
 segment readable writable
 msg db "Hello World!", 10
+msg_len = $ - msg 
 
